33af88b623392dbeaeca6d7da02f11585e9cbec9
Faustino Vasquez <vasquez.limon@gmail.com>
2026-09-08 16:00:05 -0700

    Move LazyVim lang extras to correct import order

--- nvim/lua/plugins/extras.lua
+++ nvim/lua/plugins/extras.lua
@@ -2,16 +2,6 @@ return {
   -- colorscheme: matches the "tokyo-night" theme set in Herdr
   { "LazyVim/LazyVim", opts = { colorscheme = "tokyonight" } },
 
-  -- language support
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.python" },
-  { import = "lazyvim.plugins.extras.lang.go" },
-  { import = "lazyvim.plugins.extras.lang.php" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.yaml" },
-  { import = "lazyvim.plugins.extras.lang.docker" },
-  { import = "lazyvim.plugins.extras.lang.git" },
-
   -- bash/shell: no dedicated LazyVim extra, wire it up manually
   {
     "neovim/nvim-lspconfig",
